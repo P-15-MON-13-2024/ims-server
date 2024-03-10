070f944d972a32dff21347d4d94bbb3cabad5abe
keymii <abhi.vansh2017@gmail.com>
2024-03-10 20:16:00 +0530

    add models to admin page

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
+admin.site.register(User)
+admin.site.register(Item)
+admin.site.register(Category)
+admin.site.register(IssueRecord)
 # Register your models here.
