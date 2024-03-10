ac7d1a80aa1f380bb6f0298520db91ad3b731d69
keymii <abhi.vansh2017@gmail.com>
2024-03-10 20:29:32 +0530

    minor changes

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import *
-admin.site.register(Sapiens)
+admin.site.register(Sapien)
 admin.site.register(Item)
-admin.site.register(Category)
+admin.site.register(Bucket)
 admin.site.register(IssueRecord)
 # Register your models here.
