cced674b39f93ffab389a179cacf4b57427a1925
keymii <abhi.vansh2017@gmail.com>
2024-03-14 16:32:29 +0530

    rename url

--- rfid/urls.py
+++ rfid/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('access-token/<str:scanner_uid>/', get_access_token),
     path('get-sapien/', get_sapien, name='get_sapien'),
     path('get-item/',get_item, name='get_item'),
-     path('issue-record/', add_issue_record, name='issue-record'),
+    path('add-issue-record/', add_issue_record, name='issue-record'),
 
 
 ]
