bbec34e18148cc79c9f08648e3f23a065b99c08b
Animesh <animeshkasba25@gmail.com>
2024-03-21 09:59:05 +0530

    api_for_items_inside_buckets

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import list_buckets,list_sapiens
+from .views import list_buckets,list_sapiens, list_items
 
 urlpatterns = [
-    path('categories/', list_buckets, name='list_buckets'),
+    path('buckets/', list_buckets, name='list_buckets'),
     path('sapiens/', list_sapiens, name='list_sapiens'),
+    path('items/', list_items, name='list_items'),
 ]
